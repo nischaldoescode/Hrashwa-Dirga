e792dfb51e52ee52b66597d5275a9598b5a1f530
Nischaldoescode <nischala389@gmail.com>
2026-01-05 19:36:34 +0530

    Deleted the creds

--- HrashwaDirga/src/utils/constants.ts
+++ HrashwaDirga/src/utils/constants.ts
@@ -13,7 +13,7 @@ const getApiBaseUrl = (): string => {
     if (Platform.OS === 'android') {
       // For Android physical device, use our computer's local IP
       // For emulator, you can also use this IP (works for both)
-      return 'http://192.168.1.2:5000/api';
+      return 'http://192.168.1.9:5000/api';
       
     } else if (Platform.OS === 'ios') {
       // iOS simulator can use localhost
